0e2c108eb946bcf392b7e29509bd909f7e14893d
ReyesChiew527 <reyescgl-wm23@student.tarc.edu.my>
2025-08-24 23:17:05 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "employee.crum46u0650m.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "reyes-employee1"
+customregion = "us-east-1"
+
